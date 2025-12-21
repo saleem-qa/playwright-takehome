9ef57ea2e08a270b4af621eb13aa2f400682ec0d
Saleem Malik <saleem.malik@aigilx.local>
2025-12-21 17:23:36 +0530

    Edit and duplicate handling

--- tests/todo.spec.js
+++ tests/todo.spec.js
@@ -4,36 +4,74 @@ import { test, expect } from '@playwright/test';
 test('open todo', async ({ page }) => {
     await page.goto('https://demo.playwright.dev/todomvc');
 
-//Add a new todo: "Learn Playwright".
+    //Add a new todo: "Learn Playwright".
     const todoInput = page.getByPlaceholder('What needs to be done?');
     await todoInput.fill('Learn Playwright');
     await todoInput.press('Enter');
 
-//Verify that this todo appears in the list.
+    //Verify that this todo appears in the list.
     await expect(page.getByText('Learn Playwright')).toBeVisible();
-    
-//Add a second todo: "Write tests".
+
+    //Add a second todo: "Write tests".
     await todoInput.fill('Write Tests');
     await todoInput.press('Enter');
 
-//Verify that there are 2 items in the list.
+    //Verify that there are 2 items in the list.
     const toDos = page.locator('.todo-list li');
     await expect(toDos).toHaveCount(2);
 
-//Mark "Learn Playwright" as completed & Verify The “Completed” filter shows 1 completed item.
+    //Mark "Learn Playwright" as completed & Verify The “Completed” filter shows 1 completed item.
     await page.locator('.todo-list li').filter({ hasText: 'Learn Playwright' }).locator('.toggle').check();
     await page.getByRole('link', { name: 'Completed' }).click();
     await expect(toDos).toHaveCount(1);
     await expect(page.getByText('Learn Playwright')).toBeVisible();
 
-//Verify that:The “Active” filter shows only "Write tests".
+    //Verify that:The “Active” filter shows only "Write tests".
     await page.getByRole('link', { name: 'Active' }).click();
     await expect(toDos).toHaveCount(1);
     await expect(page.getByText('Write Tests')).toBeVisible();
 
-//Clear completed items and verify the list updates correctly.
+    //Clear completed items and verify the list updates correctly.
     await page.getByRole('link', { name: 'All' }).click();
     await page.getByRole('button', { name: 'Clear completed' }).click();
     await expect(toDos).toHaveCount(1);
     await expect(page.getByText('Write Tests')).toBeVisible();
+
+    //Double click and edit
+    await page.getByText('Write Tests').dblclick();
+    const editInput = page.locator('.edit');
+    await editInput.click();
+
+    // Clear existing text
+    await editInput.press('Control+A');
+    await editInput.press('Backspace');
+
+    // Type new text
+    await editInput.fill('Complete Test');
+    await editInput.press('Enter');
+
+    //Adding Duplicate todos
+    await todoInput.fill("Tests");
+    await todoInput.press("Enter");
+    await todoInput.fill("Tests");
+    await todoInput.press("Enter");
+
+    //Selecting one todo and marking it as completed
+    const testsTodos = page.locator('.todo-list li', { hasText: 'Tests' });
+    await testsTodos.nth(1).locator('.toggle').check();
+    await page.getByRole('link', { name: 'Completed' }).click();
+    await expect(page.locator('.todo-list li')).toHaveCount(1);
+
+    //Clearing the duplicate
+    await page.getByRole('link', { name: 'All' }).click();
+    await page.getByRole('button', { name: 'Clear completed' }).click();
+
+    //Checking if duplicate is removed properly
+    await expect(toDos).toHaveCount(2);
+    await expect(toDos).toHaveText([
+        'Complete Test',
+        'Tests'
+    ]);
+
+
 });
